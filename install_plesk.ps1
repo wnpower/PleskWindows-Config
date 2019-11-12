8f33daefc76cbc30fe68bea98e3e14a68f187fd2
imorandinwnp <imorandin@wnpower.com>
2019-11-12 15:35:12 -0300

    mejoras SW-1027

--- install_plesk.ps1
+++ install_plesk.ps1
@@ -151,7 +151,17 @@ $Output = "C:\Windows\Temp\Plesk Scheduler Task #Domain Backup Scheduler 1.xml"
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile( $url , $Output)
 
-& "$env:plesk_bin\plesk.exe" "db" "INSERT INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',4,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',604800,1,1,0,NULL);"
+# BACKUP - HECHO A MANO VIENDO LA DB NO HAY INFO EN INTERNET
+& "$env:plesk_bin\plesk.exe" "db" "INSERT INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);"
+
+& "$env:plesk_bin\plesk.exe" "db" "REPLACE INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);" # Replace por si existe
+
+# Número máximo de archivos de backup completo a almacenar (incluyendo tanto los backups programados como los manuales)
+& "$env:plesk_bin\plesk.exe" "db" "UPDATE misc SET val = '30' WHERE param = 'bu_rotation';"
+
+# IONice
+& "$env:plesk_bin\plesk.exe" "db" "UPDATE misc SET val = 'true' WHERE param = 'bu_nice';"
+
 Register-ScheduledTask -Xml (get-content "C:\Windows\Temp\Plesk Scheduler Task #Domain Backup Scheduler 1.xml" | out-string) -TaskName 'Plesk Scheduler Task #Domain Backup Scheduler 1' -User "SYSTEM"
 
 echo "Configurando SQL Server..."
