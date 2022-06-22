665bfbf326325b2133096b731c7db0dbb79449cf
Ignacio Morandin <imorandin@wnpower.com>
2022-06-22 15:15:52 -0300

    update paths and packages

--- install_plesk.ps1
+++ install_plesk.ps1
@@ -39,33 +39,29 @@ echo "Ejecutando instalador de Plesk..."
 --install-component msodbcsql11 `
 --install-component msodbcsql13 `
 --install-component msodbcsql17 `
---install-component mariadb105-client `
+--install-component mariadb106-client `
 --install-component mysql-odbc53 `
 --install-component modsecurity `
---install-component php56 `
---install-component php70 `
---install-component php71 `
---install-component php72 `
---install-component php73 `
 --install-component php74 `
 --install-component php80 `
+--install-component php81 `
 --install-component webdav `
 --install-component dotnetcoreruntime `
 --install-component aspnetcore `
 --install-component appinit `
 --install-component http-dynamic-compression `
---install-component cloudflare `
 --install-component git `
+--install-component sslit `
 --install-component letsencrypt
 
 $env:plesk_dir = [System.Environment]::GetEnvironmentVariable("plesk_dir", "Machine") # REFRESH PLESK PATH
-$env:plesk_bin = $env:plesk_dir + "bin"
+$env:plesk_cli = $env:plesk_dir + "bin"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # REFRESH PATH
 
 $AdminPassword = Read-Host -Prompt 'Password usuario "Administrator" '
 
 echo "Configuracion inicial Plesk..."
-& "$env:plesk_bin\init_conf.exe" -p -passwd "$AdminPassword" -license_agreed true -admin_info_not_required true
+& "$env:plesk_cli\init_conf.exe" -p -passwd "$AdminPassword" -license_agreed true -admin_info_not_required true
 
 echo "Instalando licencia..."
 Add-Content -Path "$env:plesk_dir\admin\conf\panel.ini" -Value "[license]"
@@ -77,29 +73,29 @@ net start plesksrv
 Write-Host -NoNewLine 'En este punto instala la licencia de Plesk (XML o key) usando el panel web y luego apreta enter...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-#& "$env:plesk_bin\license.exe" -i $License
+#& "$env:plesk_cli\license.exe" -i $License
 
 echo "Configurando Plesk..."
 
 echo "Configurando Hostname..."
 $hostname = Read-Host -Prompt 'Hostname '
-& "$env:plesk_bin\server_pref.exe" --update -hostname $hostname
+& "$env:plesk_cli\server_pref.exe" --update -hostname $hostname
 
 echo "Configurando idioma..."
-& "$env:plesk_bin\locales.exe" --set-default es-ES
+& "$env:plesk_cli\locales.exe" --set-default es-ES
 
 echo "Configurando links GUI..."
-& "$env:plesk_bin\panel_gui.exe" -p -domain_registration true -cert_purchasing true
+& "$env:plesk_cli\panel_gui.exe" -p -domain_registration true -cert_purchasing true
 
 echo "Configurando servidor..."
-& "$env:plesk_bin\server_pref.exe" --update -include-remote-databases false -forbid-subscription-rename true -forbid-create-dns-subzone true -min_password_strength strong
-& "$env:plesk_bin\admin.exe" --update -multiple-sessions true
-& "$env:plesk_bin\domain_restriction.exe" --enable
-& "$env:plesk_bin\poweruser.exe" --off
+& "$env:plesk_cli\server_pref.exe" --update -include-remote-databases false -forbid-subscription-rename true -forbid-create-dns-subzone true -min_password_strength strong
+& "$env:plesk_cli\admin.exe" --update -multiple-sessions true
+& "$env:plesk_cli\domain_restriction.exe" --enable
+& "$env:plesk_cli\poweruser.exe" --off
 
 echo "Configurando IIS Pools..."
-& "$env:plesk_bin\server_pref.exe" --set-iis-app-pool-settings -cpu-usage-state true -cpu-usage-value 20 -cpu-usage-action Throttle
-& "$env:plesk_bin\server_pref.exe" -u -idle-timeout 60
+& "$env:plesk_cli\server_pref.exe" --set-iis-app-pool-settings -cpu-usage-state true -cpu-usage-value 20 -cpu-usage-action Throttle
+& "$env:plesk_cli\server_pref.exe" -u -idle-timeout 60
 
 echo "Configurando Firewall..."
 if([System.IO.File]::Exists("$Env:Programfiles (x86)\Mail Enable\Bin64\MESMTPC.exe")){
@@ -116,12 +112,12 @@ netsh advfirewall firewall add rule name="Allow OUT ICMP" protocol=icmpv4:any,an
 netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 
 echo "Configurando mail..."
-& "$env:plesk_bin\mailserver.exe" --enable-outgoing-antispam
-& "$env:plesk_bin\mailserver.exe" --set-outgoing-messages-subscription-limit 200
-& "$env:plesk_bin\mailserver.exe" --set-outgoing-messages-domain-limit 200
-& "$env:plesk_bin\mailserver.exe" --set-maps-zone "zen.spamhaus.org,bl.spamcop.net,b.barracudacentral.org"
-& "$env:plesk_bin\mailserver.exe" --set-maps-status true
-& "$env:plesk_bin\spamassassin.exe" --update-server -status true
+& "$env:plesk_cli\mailserver.exe" --enable-outgoing-antispam
+& "$env:plesk_cli\mailserver.exe" --set-outgoing-messages-subscription-limit 200
+& "$env:plesk_cli\mailserver.exe" --set-outgoing-messages-domain-limit 200
+& "$env:plesk_cli\mailserver.exe" --set-maps-zone "zen.spamhaus.org,bl.spamcop.net,b.barracudacentral.org"
+& "$env:plesk_cli\mailserver.exe" --set-maps-status true
+& "$env:plesk_cli\spamassassin.exe" --update-server -status true
 
 echo "Configurando php.ini..."
 Get-ChildItem "$env:plesk_dir\Additional\" -Recurse -Filter "php.ini" |
@@ -172,19 +168,19 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile( $url , $Output)
 
 # BACKUP - HECHO A MANO VIENDO LA DB NO HAY INFO EN INTERNET
-& "$env:plesk_bin\plesk.exe" "db" "INSERT INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);"
+& "$env:plesk_cli\plesk.exe" "db" "INSERT INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);"
 
-& "$env:plesk_bin\plesk.exe" "db" "REPLACE INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);" # Replace por si existe
+& "$env:plesk_cli\plesk.exe" "db" "REPLACE INTO backupsscheduled VALUES (1,1,'server','local','2018-07-18 10:38:16',86400,'true','false',1,'','',0,'false','true',0,'23:00:00','backup_content_all_at_domain',2592000,1,1,1,NULL);" # Replace por si existe
 
 # Número máximo de archivos de backup completo a almacenar (incluyendo tanto los backups programados como los manuales)
-& "$env:plesk_bin\plesk.exe" "db" "UPDATE misc SET val = '30' WHERE param = 'bu_rotation';"
+& "$env:plesk_cli\plesk.exe" "db" "UPDATE misc SET val = '30' WHERE param = 'bu_rotation';"
 
 # IONice
-& "$env:plesk_bin\plesk.exe" "db" "UPDATE misc SET val = 'true' WHERE param = 'bu_nice';"
+& "$env:plesk_cli\plesk.exe" "db" "UPDATE misc SET val = 'true' WHERE param = 'bu_nice';"
 
 # Carpetas excluídas
-& "$env:plesk_bin\plesk.exe" "db" "INSERT INTO backupexcludefiles VALUES (1,'/httpdocs/App_Data/cache/*\r\n/httpdocs/App_Data/tmp/*');"
-& "$env:plesk_bin\plesk.exe" "db" "REPLACE INTO backupexcludefiles VALUES (1,'/httpdocs/App_Data/cache/*\r\n/httpdocs/App_Data/tmp/*');"
+& "$env:plesk_cli\plesk.exe" "db" "INSERT INTO backupexcludefiles VALUES (1,'/httpdocs/App_Data/cache/*\r\n/httpdocs/App_Data/tmp/*');"
+& "$env:plesk_cli\plesk.exe" "db" "REPLACE INTO backupexcludefiles VALUES (1,'/httpdocs/App_Data/cache/*\r\n/httpdocs/App_Data/tmp/*');"
 
 Register-ScheduledTask -Xml (get-content "C:\Windows\Temp\Plesk Scheduler Task #Domain Backup Scheduler 1.xml" | out-string) -TaskName 'Plesk Scheduler Task #Domain Backup Scheduler 1' -User "SYSTEM"
 
@@ -224,13 +220,13 @@ if ($ISVM) {
 }
 
 echo "Configurando paquetes..."
-cmd /c '"%plesk_bin%/service_plan.exe" -c "Default Plan" -hosting true -disk_space 10G -quota 10G -max_traffic 200G -max_dom_aliases 10 -overuse block -disk_space_soft 80% -max_traffic_soft 80% -max_box -1 -mbox_quota 11G -total_mboxes_quota 10G -max_wu 100 -max_subftp_users 20 -max_mysql_db 10 -max_mssql_db 1 -mysql_dbase_space 1G -mssql_dbase_space 1G -mssql_dbase_filesize 500M -mssql_dbase_log_filesize 500M -max_maillists 0 -max_subdom 10 -max_site 1 -max_odbc 10 -max_site_builder 0 -mail true -maillist false -wuscripts true -sb_publish false -ssl true -php false -asp.net true -asp.net_version 4.0 -upsell_site_builder false -webstat awstats -err_docs true -iis_app_pool true -idle_timeout 60 -cpu_usage 20 -max_worker_processes 1 -bandwidth 256K -max_connections 100 -webmail horde -create_domains false -manage_phosting true -manage_php_settings true -manage_php_version true -manage_subdomains true -manage_domain_aliases true -manage_subftp true -manage_crontab true -manage_mail_settings true -manage_maillists false -manage_spamfilter true -manage_virusfilter false -manage_iis_app_pool true -remote_db_connection false -manage_protected_dirs true -manage_website_maintenance false -allow_local_backups true -allow_account_local_backups true -allow_ftp_backups false -allow_account_ftp_backups false -access_appcatalog false -manage_additional_permissions false -webdeploy true -asp true -write_modify false -iis_app_pool_addons false -cpu-usage-action Throttle -log-rotate true -log-bysize 10M -log-max-num-files 5 -log-compress true -keep_traf_stat 3'
+cmd /c '"%plesk_cli%/service_plan.exe" -c "Default Plan" -hosting true -disk_space 10G -quota 10G -max_traffic 200G -max_dom_aliases 10 -overuse block -disk_space_soft 80% -max_traffic_soft 80% -max_box -1 -mbox_quota 11G -total_mboxes_quota 10G -max_wu 100 -max_subftp_users 20 -max_mysql_db 10 -max_mssql_db 1 -mysql_dbase_space 1G -mssql_dbase_space 1G -mssql_dbase_filesize 500M -mssql_dbase_log_filesize 500M -max_maillists 0 -max_subdom 10 -max_site 1 -max_odbc 10 -max_site_builder 0 -mail true -maillist false -wuscripts true -sb_publish false -ssl true -php false -asp.net true -asp.net_version 4.0 -upsell_site_builder false -webstat awstats -err_docs true -iis_app_pool true -idle_timeout 60 -cpu_usage 20 -max_worker_processes 1 -bandwidth 256K -max_connections 100 -webmail horde -create_domains false -manage_phosting true -manage_php_settings true -manage_php_version true -manage_subdomains true -manage_domain_aliases true -manage_subftp true -manage_crontab true -manage_mail_settings true -manage_maillists false -manage_spamfilter true -manage_virusfilter false -manage_iis_app_pool true -remote_db_connection false -manage_protected_dirs true -manage_website_maintenance false -allow_local_backups true -allow_account_local_backups true -allow_ftp_backups false -allow_account_ftp_backups false -access_appcatalog false -manage_additional_permissions false -webdeploy true -asp true -write_modify false -iis_app_pool_addons false -cpu-usage-action Throttle -log-rotate true -log-bysize 10M -log-max-num-files 5 -log-compress true -keep_traf_stat 3'
 echo "Activando Lets Encrypt en paquete default..."
-cmd /c '"%plesk_bin%/service_plan.exe" --add-custom-plan-item "Default Plan" -custom-plan-item-name "urn:ext:letsencrypt:plan-item-sdk:keep-secured"'
+cmd /c '"%plesk_cli%/service_plan.exe" --add-custom-plan-item "Default Plan" -custom-plan-item-name "urn:ext:letsencrypt:plan-item-sdk:keep-secured"'
 
 echo "Eliminando paquetes adicionales..."
-& "$env:plesk_bin\service_plan.exe" --remove "Default Simple"
-& "$env:plesk_bin\service_plan.exe" --remove "Unlimited"
+& "$env:plesk_cli\service_plan.exe" --remove "Default Simple"
+& "$env:plesk_cli\service_plan.exe" --remove "Unlimited"
 
 echo "Configurando MailEnable..."
 $hostname = [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
@@ -245,10 +241,10 @@ echo "Configurando tiempo de session Plesk..."
 plesk db "update misc set val=600 where param='login_timeout'"
 
 echo "Configurando notificaciones SSL..."
-& "$env:plesk_bin\notification.exe" --update -code ext-letsencrypt-notification-certificateAutoRenewalSucceed -send2admin false -send2reseller false -send2client false -send2email false
-& "$env:plesk_bin\notification.exe" --update -code ext-letsencrypt-notification-certificateAutoRenewalFailed -send2admin false -send2reseller false -send2client false -send2email false
-& "$env:plesk_bin\notification.exe" --update -code ext-sslit-notification-certificateAutoRenewalSucceed -send2admin false -send2reseller false -send2client false -send2email false
-& "$env:plesk_bin\notification.exe" --update -code ext-sslit-notification-certificateAutoRenewalFailed -send2admin false -send2reseller false -send2client false -send2email false
+& "$env:plesk_cli\notification.exe" --update -code ext-letsencrypt-notification-certificateAutoRenewalSucceed -send2admin false -send2reseller false -send2client false -send2email false
+& "$env:plesk_cli\notification.exe" --update -code ext-letsencrypt-notification-certificateAutoRenewalFailed -send2admin false -send2reseller false -send2client false -send2email false
+& "$env:plesk_cli\notification.exe" --update -code ext-sslit-notification-certificateAutoRenewalSucceed -send2admin false -send2reseller false -send2client false -send2email false
+& "$env:plesk_cli\notification.exe" --update -code ext-sslit-notification-certificateAutoRenewalFailed -send2admin false -send2reseller false -send2client false -send2email false
 
 echo "Reparando CVE MyLittleAdmin..."
 # https://support.plesk.com/hc/en-us/articles/360013996240-CVE-2020-13166-myLittleAdmin-vulnerability
@@ -259,7 +255,7 @@ $xml.SelectNodes("//machineKey[.]") | % { $_.ParentNode.RemoveChild($_) }
 $xml.save("$env:PLESK_DIR\MyLittleAdmin\web.config")
 
 echo "Desactivando reglas de Firewall..."
-Disable-NetFirewallRule -DisplayName "MariaDB 10.5 (x64)"
+Disable-NetFirewallRule -DisplayName "MariaDB 10.6 (x64)"
 Disable-NetFirewallRule -DisplayName "Client MySQL server"
 
 echo "Instalando SQL Management Studio..."
