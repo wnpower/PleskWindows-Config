79c7d69ac7f9c942646c537c1fa439b4dd164131
Ignacio Morandin <imorandin@wnpower.com>
2024-04-25 13:28:53 -0300

    cambio instalador choco

--- install_plesk.ps1
+++ install_plesk.ps1
@@ -4,7 +4,7 @@ $ISVM = (Get-WmiObject -Class Win32_ComputerSystem).Model | Select-String -Patte
 
 echo "Instalando dependencias..."
 echo "Instalando Chocolatey..."
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # REFRESH PATH
 
